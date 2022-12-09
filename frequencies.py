bf2b7c3c74efbfb2b35d6b021b51c65605ecef08
r-nidhu31 <r.nidhu31@Gmail.com>
2022-12-09 00:11:37 +0000

    finished

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,16 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    visitedItems = []
+
+    for x in items:
+        if(not (str(x) in visitedItems)):
+            frequency = 0;
+            frequencies[str(x)] = frequency;
+            visitedItems.append(str(x))
+            for y in items:
+                if(str(y) == str(x)):
+                    frequency = frequency + 1;
+                    frequencies[str(x)] = frequency;
+    
     return frequencies
